411a71e47f6c39ea379aa936517c85511fdb6650
Josh Nylander <joshn@ridewta.com>
2019-09-15 21:11:17 -0700

    re-org

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ Import-Module PowerShellGet
 #Publish to PSGallery and install/import locally
 
 Publish-Module -Path .\AMAG-SMSPowershell -Repository PSGallery -Verbose
-Install-Module -Name AMAG-SMSPowershell -Force
-Import-Module -Name AMAG-SMSPowershell -Force
+Install-Module -Name AMAG-SMSPowershell -Repository PSGallery -Force
+Import-Module -Name AMAG-SMSPowershell -Force 
